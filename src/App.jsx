b4ee0fc6a10c62065010c8357068b9322f57d407
PawanAK <pawanajjark@gmail.com>
2023-05-17 16:05:36 +0530

    Strarting Redux Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Profile />
+      <Login />
+    </>
+  );
 }
 
 export default App;
